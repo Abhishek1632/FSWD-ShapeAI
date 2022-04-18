ba80d1a76c0109195fca0541945f70204136e713
Abhishek1632 <91139469+Abhishek1632@users.noreply.github.com>
2022-04-18 22:13:32 +0530

    Promises

---
+++ images/app18.js
@@ -0,0 +1,8 @@
+var body = document.querySelector("body");
+var button = document.querySelector("button");
+
+const callbackfunc = (event) => {
+  body.classList.toggle("dark");
+};
+
+button.addEventListener("click", callbackfunc);
