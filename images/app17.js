c1c877434d186ef03f27fa596e9b0a4175bce669
Abhishek1632 <91139469+Abhishek1632@users.noreply.github.com>
2022-04-18 22:05:05 +0530

    append child

---
+++ images/app17.js
@@ -0,0 +1,12 @@
+var list = document.querySelector("ul");
+
+var arr = ["Go the GYM ", "cook oatmeal", "Eat"];
+
+arr.push("Complete the assignment");
+
+for (var i = 0; i < arr.length; i++) {
+  var element = document.createElement("li");
+  var textNode = document.createTextNode(arr[i]);
+  element.appendChild(textNode);
+  list.appendChild(element);
+}
